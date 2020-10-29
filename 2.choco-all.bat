f874e729f7ca50b0170e2ad4f888d154fce22cae
Hung-Su Nguyen <hungsu.nguyen@gmail.com>
2020-10-28 22:03:23 -0700

    Update and rename choco-all.bat to 2.choco-all.bat

---
+++ 2.choco-all.bat
@@ -0,0 +1,12 @@
+choco install firefox
+choco install discord
+choco install bitwarden
+choco install vscode
+choco install figma
+choco install fiddler
+choco install googlechrome
+choco install treesizefree
+choco install slack
+choco install steam
+choco install zoom
+choco install 7zip
